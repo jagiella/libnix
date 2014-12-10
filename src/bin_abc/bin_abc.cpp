44cd0d014d8af9e30e5b48d8d1687e01c23b7f4b
Nick Jagiella <nick.jagiella@gmail.com>
2014-12-10 15:28:03 +0100

    External function: force file piping
    - removed possibility to directly read-out stdout of external function
    calls

--- src/bin_abc/bin_abc.cpp
+++ src/bin_abc/bin_abc.cpp
@@ -96,9 +96,9 @@ int main( int argc, char **argv){
 	  		}else{
 	  			fprintf( stderr, "Use user-provided command [ %s ]\n", optarg);
 	  			func_data = optarg;
-	  #if __linux
+/*	  #if __linux
 	  			func = externalfuncPipe;
-	  #else
+	  #else*/
 	  			func = externalfuncFile;
 	  #endif
 	  		}
